8a31d579009e38aaa7185c24867ec03dfbd3fa3e
ddd-cyter <82017283+ddd-cyter@users.noreply.github.com>
2025-03-20 22:44:51 +0800

    Update streamlit_app.py
    perf

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,3 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.header("hello wwwww")
-container=st.container()
-
-if "messages" not in st.session_state:
-    st.session_state.messages=[]
-
-prompt=st.chat_input('请输入您要问的问题：')
-if prompt:
-    st.session_state.messages.append(prompt)
-
-with container:
-    with st.chat_message('user'):
-        for message in st.session_state.messages:
-            st.write(message)
-         
